ef0fd591bcb284b3cf07ae2155b030bd3a34e0e9
Andres Outon <outon.x@gmail.com>
2018-08-25 22:31:38 -0500

    refactored some code on the CardList component

--- src/CardList.js
+++ src/CardList.js
@@ -3,19 +3,22 @@ import React from "react";
 import Card from "./Card";
 
 const CardList = ({ plants }) => {
-  const cardComponent = plants.map((plant, i) => {
-    return (
-      <Card
-        key={i}
-        id={plants[i].id}
-        name={plants[i].name}
-        type={plants[i].type}
-        germTime={plants[i].harvSeason}
-        harvTime={plants[i].harvTime}
-      />
-    );
-  });
-  return <div>{cardComponent}</div>;
+  return (
+    <div>
+      {plants.map((plant, i) => {
+        return (
+          <Card
+            key={i}
+            id={plants[i].id}
+            name={plants[i].name}
+            type={plants[i].type}
+            germTime={plants[i].harvSeason}
+            harvTime={plants[i].harvTime}
+          />
+        );
+      })}
+    </div>
+  );
 };
 
 export default CardList;
