28fc17a2b8ac978b725eb9a3cc1406c18033655e
Andres Outon <outon.x@gmail.com>
2018-08-25 22:00:02 -0500

    worked on initial card component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Card from "./Card";
+import registerServiceWorker from "./registerServiceWorker";
+import "tachyons";
+import { plants } from "./plants";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <div>
+    <Card
+      id={plants[1].id}
+      name={plants[1].name}
+      type={plants[1].type}
+      germTime={plants[1].germTime}
+      harvTime={plants[0].harvTime}
+    />
+    <Card
+      id={plants[2].id}
+      name={plants[2].name}
+      type={plants[2].type}
+      germTime={plants[2].germTime}
+      harvTime={plants[1].harvTime}
+    />
+    <Card
+      id={plants[0].id}
+      name={plants[0].name}
+      type={plants[0].type}
+      germTime={plants[0].germTime}
+      harvTime={plants[2].harvTime}
+    />
+  </div>,
+  document.getElementById("root")
+);
 registerServiceWorker();
---
+++ src/plants.js
@@ -0,0 +1,23 @@
+export const plants = [
+  {
+    id: 1,
+    name: "Watermelon",
+    type: "Fruit",
+    germTime: "2.5 weeks",
+    harvTime: "6 weeks"
+  },
+  {
+    id: 2,
+    name: "Bannanna",
+    type: "Fruit",
+    germTime: "1 week",
+    harvTime: "4 - 5 weeks"
+  },
+  {
+    id: 3,
+    name: "Brocoli",
+    type: "Vegetable",
+    germTime: ".5 week",
+    harvTime: "3.5 weeks"
+  }
+];
