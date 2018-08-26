a48d88f42da5e9bf9701c4ac1f44c17e03cae2ed
Andres Outon <outon.x@gmail.com>
2018-08-25 22:37:24 -0500

    added App component to be the parent of all future components/modules

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import CardList from "./CardList";
+import { plants } from "./plants";
+
+const App = () => {
+  return <CardList plants={plants} />;
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import CardList from "./CardList";
 import registerServiceWorker from "./registerServiceWorker";
 import "tachyons";
-import { plants } from "./plants";
+import App from "./App.js";
 
-ReactDOM.render(<CardList plants={plants} />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
