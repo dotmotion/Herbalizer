1cd106f6d3aa6f395675e5e85492337cb0c7e5a9
Andres Outon <outon.x@gmail.com>
2018-08-25 22:47:56 -0500

    added new classes to components to center the elements and added a search bar for later

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from "react";
 import CardList from "./CardList";
+import SearchBox from "./SearchBox.js";
 import { plants } from "./plants";
 
 const App = () => {
-  return <CardList plants={plants} />;
+  return (
+    <div className="tc">
+      <h1>My Garden</h1>
+      <SearchBox />
+      <CardList plants={plants} />
+    </div>
+  );
 };
 
 export default App;
--- src/Card.js
+++ src/Card.js
@@ -3,7 +3,7 @@ import code from "./code.svg";
 
 const Card = ({ name, type, germTime, harvTime }) => {
   return (
-    <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+    <div className="bg-dark-green dib br3 pa3 ma2 grow bw2 shadow-5">
       <img alt="plant" src={code} width="200px" height="200px" />
       <div>
         <h2>{name}</h2>
