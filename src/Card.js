c5731cb416c10244c3bcf7535eeefa5154e913ff
Andres Outon <outon.x@gmail.com>
2018-08-27 19:24:42 -0500

    changed card format

--- src/Card.js
+++ src/Card.js
@@ -4,15 +4,15 @@ import code from "./code.svg";
 const Card = ({ name, type, epSiembra, tCosecha }) => {
   return (
     <div className="bg-dark-green dib br3 pa3 ma2 grow bw2 shadow-5">
-      <img alt="plant" src={code} width="200px" height="200px" />
+      <img alt="plant" src={code} width="100px" height="100px" />
       <div>
         <h2>{name}</h2>
         <p>
-          <span className="b">Epoca de Siembra: </span>
+          <span className="b">Siembra: </span>
           {epSiembra}
         </p>
         <p>
-          <span className="b">Tiempo de Cosecha: </span>
+          <span className="b">Cosecha: </span>
           {tCosecha}
         </p>
       </div>
