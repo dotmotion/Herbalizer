13b25af5111a387590004002ae7b631dc72ed0c5
Andres Outon <outon.x@gmail.com>
2018-08-27 21:20:42 -0500

    added new styles

--- src/CardList.js
+++ src/CardList.js
@@ -4,7 +4,7 @@ import Card from "./Card";
 
 const CardList = ({ plants }) => {
   return (
-    <div>
+    <div className="cardList">
       {plants.map((plant, i) => {
         return (
           <Card
--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,15 +1,23 @@
 import React from "react";
+import leaf from "./leaf.svg";
 
 const SearchBox = ({ searchField, searchChange }) => {
   return (
-    <div className="pa2">
+    <form class="search-container tc">
       <input
+        id="search-box"
+        type="text"
+        class="search-box"
+        name="q"
         onChange={searchChange}
-        type="search"
-        className=""
-        placeholder="Search Plants"
       />
-    </div>
+      <label for="search-box">
+        <span>
+          <img src={leaf} className="search-icon" alt="search" />
+        </span>
+      </label>
+      <input type="submit" id="search-submit" />
+    </form>
   );
 };
 
